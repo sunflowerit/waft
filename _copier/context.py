6de35e7bb10d669c6bcc3397fbd3d67a5795c21b
Tom <tblauwendraat@therp.nl>
2026-01-28 10:15:50 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [ADD] context hook for defaults

--- _copier/context.py
+++ _copier/context.py
@@ -39,5 +39,4 @@ class ContextUpdater(ContextHook):
             context_updates["default_waftlib_version"] = self.waftlib_version
             context_updates["default_odoo_version"] = self.odoo_version
             self.detection_run = True
-        print(context_updates)
         return context_updates
