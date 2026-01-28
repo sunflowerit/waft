2c5145c8a2ecee87ac27e11c870617002b4a8a73
Tom <tblauwendraat@therp.nl>
2026-01-28 10:07:36 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [ADD] context hook for defaults

--- _copier/context.py
+++ _copier/context.py
@@ -6,10 +6,10 @@ from copier_template_extensions import ContextHook
 class ContextUpdater(ContextHook):
 
     detection_run = False
-    default_waft_version = None
-    default_odoo_version = None
+    waftlib_version = None
+    odoo_version = None
 
-    def _detect_waft_version(self):
+    def _detect_waftlib_version(self):
         with open("bootstrap", "r") as file:
             for line in file:
                 if line.startswith("export LIBRARIES_VERSION_BRANCH"):
@@ -33,11 +33,11 @@ class ContextUpdater(ContextHook):
         if context.get("_copier_phase") == "prompt":
             if not self.detection_run:
                 print("Trying to detect waftlib version from bootstrap file...")
-                self.default_waftlib_version = self._detect_waft_version()
+                self.waftlib_version = self._detect_waftlib_version()
                 print("Trying to detect Odoo version from .env-shared file...")
-                self.default_odoo_version = self._detect_odoo_version()
-            context_updates["default_waftlib_version"] = self.default_waftlib_version
-            context_updates["default_odoo_version"] = self.default_odoo_version
+                self.odoo_version = self._detect_odoo_version()
+            context_updates["default_waftlib_version"] = self.waftlib_version
+            context_updates["default_odoo_version"] = self.odoo_version
             self.detection_run = True
-            
+        print(context_updates)
         return context_updates
