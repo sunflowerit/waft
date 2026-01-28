397d0dcd3c58a9632871a4c49b0175974c06022c
Tom <tblauwendraat@therp.nl>
2026-01-28 09:56:48 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! [ADD] context hook for defaults

--- _copier/context.py
+++ _copier/context.py
@@ -4,27 +4,40 @@ from copier_template_extensions import ContextHook
 
 
 class ContextUpdater(ContextHook):
-    def hook(self, context):
-        context_updates = {}
 
-        print("Trying to detect waftlib version from bootstrap file...")
+    detection_run = False
+
+    def _detect_waft_version(self):
         with open("bootstrap", "r") as file:
             for line in file:
                 if line.startswith("export LIBRARIES_VERSION_BRANCH"):
                     pattern = r':-(.+?)}'
                     _match = re.search(pattern, line)
                     if _match:
-                        context_updates["default_waftlib_version"] = _match.group(1)
-                        break
+                        return _match.group(1)
 
-        print("Trying to detect Odoo version from .env-shared file...")
+    def _detect_odoo_version(self):
         with open(".env-shared", "r") as file:
             for line in file:
                 if line.startswith("ODOO_VERSION="):
                     pattern = r'=\"(.+?)\"'
                     _match = re.search(pattern, line)
                     if _match:
-                        context_updates["default_odoo_version"] = _match.group(1)
-                        break
+                        return _match.group(1)
 
+    def hook(self, context):
+        context_updates = {}
+
+        print(context["_copier_phase"])
+        print(context["default_waft_version"])
+        print(context["default_odoo_version"])
+        if not self.detection_run:
+            print("Trying to detect waftlib version from bootstrap file...")
+            waftlib_version = self._detect_waft_version()
+            context_updates["default_waftlib_version"] = waftlib_version
+            print("Trying to detect Odoo version from .env-shared file...")
+            odoo_version = self._detect_odoo_version()
+            context_updates["default_odoo_version"] = odoo_version
+            self.detection_run = True
+            
         return context_updates
